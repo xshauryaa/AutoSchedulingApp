79b9361dca40b1aa1af33d6165648f1961db24ac
Shaurya Thareja <shauryathareja19@gmail.com>
2025-01-22 11:54:49 -0800

    Added specification for FluidEvent class

--- src/main/model/FluidEvent.java
+++ src/main/model/FluidEvent.java
@@ -1,5 +1,35 @@
 package model;
 
+import java.util.Date;
+
 public class FluidEvent extends Event {
     
+    private Date deadline; // The deadline of the event
+
+    /**
+     * Creates a new event on given date with given name, activity type, priority, duration, and deadline.
+     * @param name the name of the event
+     * @param date the date of the event
+     * @param type the type of activity of the event
+     * @param priority the priority of the event
+     * @param duration the duration of the event in minutes
+     * @param deadline the deadline of the event
+     */
+    public FluidEvent(String name, Date date, ActivityType type, Priority priority, int duration, Date deadline) {
+        // TODO: Implement constructor
+    }
+
+    /**
+     * @return the deadline of the event.
+     */
+    public Date getDeadline() {
+        return null; // TODO: Implement method
+    }
+
+    /**
+     * @param deadline the new deadline of the event
+     */
+    public void setDeadline(Date deadline) {
+        // TODO: Implement method
+    }
 }
