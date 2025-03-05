cc43218f40c1469626ef6e76d04d807c58fae5c1
Shaurya Thareja <shauryathareja19@gmail.com>
2025-03-05 15:05:40 -0800

    Added specification for EventDependencies class

--- src/main/model/EventDependencies.java
+++ src/main/model/EventDependencies.java
@@ -1,5 +1,26 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class EventDependencies {
     
+    private HashMap<Event, ArrayList<Event>> dependencies;
+
+    public EventDependencies() {
+        // TODO: implement constructor
+    }
+
+    public void addDependency(Event event, Event dependency) {
+        // TODO: implement method
+    }
+
+    public void removeDependency(Event event, Event dependency) {
+        // TODO: implement method
+    }
+
+    public ArrayList<Event> getDependenciesForEvent(Event event) {
+        // TODO: implement method
+        return null;
+    }
 }
